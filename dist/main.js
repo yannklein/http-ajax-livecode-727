12d5c8d147490d3c4f083ba70c79593ffd7d61b9
Yann Klein <yann.klein@me.com>
2021-11-03 18:08:11 +0900

    livecode done

--- dist/main.js
+++ dist/main.js
@@ -95,44 +95,49 @@
 
 const authorization = "Bearer sk_f9deb0090ac7688f94eb11ce09fd6a6e";
 
+// Pseudocode
 
-// 1. Select the submit button and the input
-const button = document.querySelector("#clearbitSubmit");
-const input = document.querySelector("#clearbitEmail");
-const userName = document.querySelector("#userName");
-const userEmail = document.querySelector("#userEmail");
-const userBio = document.querySelector("#userBio");
-const userLocation = document.querySelector("#userLocation");
+// 1. Select submit button, form area, results area, userName, userEmail, userBio, userLocation
+const submit = document.querySelector("#clearbitSubmit");
+const userText = document.querySelector("#clearbitEmail");
+const name = document.querySelector("#userName");
+const email = document.querySelector("#userEmail");
+const bio = document.querySelector("#userBio");
+const location = document.querySelector("#userLocation");
 
-const displayInfo = (person) => {
-  userName.innerText = person.name.fullName;
-  userBio.innerText = person.bio;
-  userEmail.innerText = person.email;
-  userLocation.innerText = person.location;
+const displayData = (person) => {
+  name.innerText = person.name.fullName;
+  email.innerText = person.email;
+  bio.innerText = person.bio;
+  location.innerText = person.location;
 };
 
-const fetchClearbit = (email) => {
-  const url = `https://person.clearbit.com/v2/combined/find?email=${email}`
-  fetch(url, {headers: {Authorization: authorization}})
-  .then(response => response.json())
-  .then((data) => {
-    const person = data.person;
-    // 3. change the DOM (each td)
-    displayInfo(person);
-  });
+const fetchUserData = (email) => {
+  const url = `https://person-stream.clearbit.com/v2/combined/find?email=${email}`;
+  fetch(url, {
+    headers: {'Authorization': authorization}
+  })
+    .then(response => response.json())
+    .then((data)=> {
+      console.log(data);
+      const person = data.person;
+      displayData(person);
+    });
 };
 
-const stalkAPerson = (event) => {
-  // PREVENT THAT MF DEFAULT
+const stalkSomeone = (event) => {
+  // preventdefault()?
   event.preventDefault();
-  const email = input.value;
-  // 2.5 fetch from the API
-  fetchClearbit(email);
+  // 2.5 fetch(url)
+  fetchUserData(userText.value);
 };
 
-// 2. Listen for a click
-button.addEventListener("click", stalkAPerson);
+// take out the contents 
+// 2. submit event listener to the submit button
+submit.addEventListener("click", stalkSomeone);
 
+// 3. change the DOM 
+// send the datas to results(tds)
 
 
 /***/ })
